a89ca1dd77292de6219735938f225d6bd21b488b
Hirotaka Mizutani <hirotaka@mizutani.to>
2020-09-10 13:00:20 +0900

    Add .config

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+# https://qiita.com/hennin/items/33758226a0de8c963ddf
+set -U FZF_LEGACY_KEYBINDINGS 0
+
+# oh-my-fish/theme-bobthefish
+set -g theme_display_date no
+set -g theme_display_cmd_duration no
+set -g theme_color_scheme zenburn
+
+# anyenv
+set -x PATH ~/.anyenv/bin $PATH
+anyenv init - fish | source
