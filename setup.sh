c1fccf8c3be29cc17fde52e23f9a6ac634dc9cb0
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2023-04-27 11:06:15 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ echo "done"
 (
 	echo
 	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-) >>/Users/hirotaka/.zprofile
+) >>~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # packages
@@ -60,41 +60,3 @@ echo "done"
 # cask-fonts
 brew tap homebrew/cask-fonts
 brew install --cask font-sauce-code-pro-nerd-font
-
-# dotfiles
-echo "Setting up dotfiles..."
-if [ ! -d ~/Workspaces/github.com/hirotaka/dotfiles ]; then
-	mkdir -p ~/Workspaces/github.com/hirotaka
-	cd ~/Workspaces/github.com/hirotaka
-	git clone git@github.com:hirotaka/dotfiles.git
-fi
-if [ ! -f ~/.tmux.conf ]; then
-	ln -s ~/Workspaces/github.com/hirotaka/dotfiles/.tmux.conf ~/.tmux.conf
-fi
-if [ ! -f ~/.config ]; then
-	ln -s ~/Workspaces/github.com/hirotaka/dotfiles/.config ~/.config
-fi
-
-# asdf
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-
-# fish
-echo "Setting up fish..."
-if [ $SHELL != '/usr/local/bin/fish' ] && [ ! -f /usr/local/bin/fish ]; then
-	echo $(which fish) | sudo tee -a /etc/shells
-	chsh -s $(which fish)
-	echo "done"
-else
-	echo "fish is already setted up or could not find fish"
-fi
-
-# Atok
-echo "Download file from https://mypassport.atok.com/install/install_mac.html"
-
-# terminfo
-# /opt/homebrew/opt/ncurses/bin/infocmp tmux-256color > ~/tmux-256color.info
-# tic -xe tmux-256color ~/.config/tmux/tmux-256color.info
-
-# ssh-agent
-# ssh-add --apple-use-keychain ~/.ssh/id_rsa
