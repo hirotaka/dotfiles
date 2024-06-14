1d9167f29953e4f9b351453ca5a146533a625434
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2024-06-14 15:40:38 +0900

    Update config: raycast

---
+++ raycast/.config/raycast/scripts/emacs-org-capture.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Emacs org-capture
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+/opt/homebrew/bin/emacs
