0ad4f94691ec1e603faf9bdcb5dbacce4f6b4fa5
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2024-07-19 10:10:51 +0900

    Update config: nvim

--- nvim/.config/nvim/lua/plugins/tmp.lua
+++ nvim/.config/nvim/lua/plugins/tmp.lua
@@ -1,5 +1,29 @@
 return {
   {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>-",
+        function()
+          require("yazi").yazi()
+        end,
+        desc = "Open the file manager",
+      },
+      {
+        "<leader>cw",
+        function()
+          require("yazi").yazi(nil, vim.fn.getcwd())
+        end,
+        desc = "Open the file manager in nvim's working directory",
+      },
+    },
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+    },
+  },
+  {
     "garymjr/nvim-snippets",
     dependencies = {
       "rafamadriz/friendly-snippets",
