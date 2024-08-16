1e932bf53ab5e9b841963fc2c76f2f4555f2de39
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2024-08-16 11:23:34 +0900

    Update config: jq

--- nvim/.config/nvim/bin/neotest-rspec.sh
+++ nvim/.config/nvim/bin/neotest-rspec.sh
@@ -3,17 +3,18 @@
 # Customize the following:
 # - escaped_path: absolute local path to your project
 # - container: name of your docker container
-escaped_path="\/Users\/hirotaka\/Workspaces\/github.com\/GuildWorks\/eyan\/" # Be careful to properly escape this
+# escaped_path="\/Users\/hirotaka\/Workspaces\/github.com\/GuildWorks\/eyan\/" # Be careful to properly escape this
+escaped_path="\/Users\/hirotaka\/Workspaces\/github.com\/GuildWorks\/eyan-renamed\/" # Be careful to properly escape this
 container="web"
 
 # WARN: This will break if flags other than -o and -f are added in neotest-rspec
 while getopts o:f: flag; do
-	# This deliberately does not handle all arguments
-	# shellcheck disable=SC2220
-	# shellcheck disable=SC2213
-	case "${flag}" in
-	o) output_path=${OPTARG} ;;
-	esac
+  # This deliberately does not handle all arguments
+  # shellcheck disable=SC2220
+  # shellcheck disable=SC2213
+  case "${flag}" in
+  o) output_path=${OPTARG} ;;
+  esac
 done
 
 # Strip local path from test paths sent to container
