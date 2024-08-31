df46ca9e2483df0904e14f45500dc1a62b38cc71
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2024-08-31 18:06:38 +0900

    Update config: doom

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -28,9 +28,10 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "PlemolJP35 Console NF" :size 13 :weight 'medium)
-      doom-variable-pitch-font (font-spec :family "PlemolJP Console NF" :size 13)
-      doom-symbol-font (font-spec :family "PlemolJP Console NF" :size 13))
+;; Moralerspace Argon NF
+(setq doom-font (font-spec :family "Moralerspace Argon NF" :size 13 :weight 'medium)
+      doom-variable-pitch-font (font-spec :family "Moralerspace Argon NF" :size 13)
+      doom-symbol-font (font-spec :family "Moralerspace Argon NF" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
