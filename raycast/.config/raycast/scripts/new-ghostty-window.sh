8552c5e3c4a6570644dc75b73e33c737be85b00e
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2024-12-28 20:30:12 +0900

    Update config: raycast

---
+++ raycast/.config/raycast/scripts/new-ghostty-window.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title New Ghostty Window
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 👻
+#
+open -n "/Applications/Ghostty.app"
