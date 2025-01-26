76961a2fd1a711b3900083939846b3c978a200c8
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2025-01-26 14:20:57 +0900

    Update config: fish

--- fish/.config/fish/conf.d/zellij_abbr.fish
+++ fish/.config/fish/conf.d/zellij_abbr.fish
@@ -3,3 +3,13 @@
 abbr ze zellij
 abbr zel 'zellij ls'
 abbr zea 'zellij attach'
+
+function zellij_new_session
+    set -l dir $argv[1]
+    set -l session (string replace '/' '-' $dir)
+
+    z $dir
+    zellij --session $session --new-session-with-layout $argv[2]
+end
+
+alias zen zellij_new_session
